52daf43a80051a8643fddba1f423daed07f72a70
uuuuuuum <youmechoi9@gmail.com>
2022-10-10 20:33:39 +0900

    Edit: ui update

--- src/pages/OrderPage/OrderPage.js
+++ src/pages/OrderPage/OrderPage.js
@@ -3,9 +3,21 @@ import Type from "./Type";
 
 function OrderPage() {
   return (
-    <div>
-      <Type orderType="products" />
-      <Type orderType="options" />
+    <div style={{ padding: "20px", textAlign: "left" }}>
+      <h1>Travel Products</h1>
+      <div>
+        <Type orderType="products" />
+      </div>
+      <div style={{ display: "flex", marginTop: 20 }}>
+        <div style={{ width: "50%" }}>
+          <Type orderType="options" />
+        </div>
+        <div>
+          <h2>Total Price:</h2>
+          <br />
+          <button>주문</button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/OrderPage/Type.js
+++ src/pages/OrderPage/Type.js
@@ -38,5 +38,19 @@ export default function Type({ orderType }) {
     );
   });
 
-  return <div>{optionItems}</div>;
+  return (
+    <>
+      <h2>주문 종류</h2>
+      <p> 하나의 가격</p>
+      <p> 총 가격:</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: orderType === "options" && "column",
+        }}
+      >
+        {optionItems}
+      </div>
+    </>
+  );
 }
