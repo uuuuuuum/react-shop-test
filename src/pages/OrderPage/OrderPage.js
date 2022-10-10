1be2104281b8d23ee9a87b0aa1358fd8932bc3f8
uuuuuuum <youmechoi9@gmail.com>
2022-10-10 22:46:53 +0900

    Add: test that add and remove total price

--- src/pages/OrderPage/OrderPage.js
+++ src/pages/OrderPage/OrderPage.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { OrderContext } from "../../contexts/OrderContext";
 import Type from "./Type";
 
 function OrderPage() {
+  const [orderDatas] = useContext(OrderContext);
   return (
     <div style={{ padding: "20px", textAlign: "left" }}>
       <h1>Travel Products</h1>
@@ -13,7 +15,7 @@ function OrderPage() {
           <Type orderType="options" />
         </div>
         <div>
-          <h2>Total Price:</h2>
+          <h2>Total Price: {orderDatas.totals.total}</h2>
           <br />
           <button>주문</button>
         </div>
--- src/pages/OrderPage/tests/calculate.test.js
+++ src/pages/OrderPage/tests/calculate.test.js
@@ -2,6 +2,7 @@ import { render, screen } from "../../../test-utils";
 import userEvent from "@testing-library/user-event";
 import { OrderContextProvider } from "../../../contexts/OrderContext";
 import Type from "../Type";
+import OrderPage from "../OrderPage";
 
 test("update product's total when products change", async () => {
   render(<Type orderType="products" />);
@@ -41,4 +42,52 @@ test("update option's total when options change", async () => {
 
   userEvent.click(dinnerCheckbox);
   expect(optionsTotal).toHaveTextContent("500");
+});
+
+describe("total price of goods and options", () => {
+  test("total price starts with 0 and Updating total price when adding one product", async () => {
+    render(<OrderPage />);
+    const total = screen.getByText("Total Price:", { exact: false });
+    expect(total).toHaveTextContent("0");
+
+    const americaInput = await screen.findByRole("spinbutton", {
+      name: "America",
+    });
+    userEvent.clear(americaInput);
+    userEvent.type(americaInput, "1");
+
+    expect(total).toHaveTextContent("1000");
+  });
+
+  test("Updating total price when adding one option", async () => {
+    render(<OrderPage />);
+    const total = screen.getByText("Total Price:", { exact: false });
+
+    const insuranceCheckbox = await screen.findByRole("checkbox", {
+      name: "Insurance",
+    });
+    userEvent.click(insuranceCheckbox);
+    expect(total).toHaveTextContent("500");
+  });
+
+  test("Updating total price when removing option and product", async () => {
+    render(<OrderPage />);
+    const total = screen.getByText("Total Price", { exact: false });
+
+    const insuranceCheckbox = await screen.findByRole("checkbox", {
+      name: "Insurance",
+    });
+    userEvent.click(insuranceCheckbox);
+
+    const americaInput = await screen.findByRole("spinbutton", {
+      name: "America",
+    });
+    userEvent.clear(americaInput);
+    userEvent.type(americaInput, "3");
+
+    userEvent.clear(americaInput);
+    userEvent.type(americaInput, "1");
+
+    expect(total).toHaveTextContent("1500");
+  });
 });
