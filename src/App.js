361850f8c141b6a951f40a435f927f1a373a4a90
uuuuuuum <youmechoi9@gmail.com>
2022-10-11 00:42:39 +0900

    Add: step to each page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 import SummaryPage from "./pages/SummaryPage/SummaryPage";
 import OrderPage from "./pages/OrderPage/OrderPage";
+import CompletePage from "./pages/CompletePage/CompletePage";
 import { OrderContextProvider } from "./contexts/OrderContext";
 
 function App() {
+  const [step, setStep] = useState(0);
   return (
-    <div className="App">
-      {/* <SummaryPage /> */}
+    <div className="App" style={{ padding: "4rem" }}>
       <OrderContextProvider>
-        <OrderPage />
+        {step === 0 && <OrderPage setStep={setStep} />}
+        {step === 1 && <SummaryPage setStep={setStep} />}
+        {step === 2 && <CompletePage setStep={setStep} />}
       </OrderContextProvider>
     </div>
   );
--- src/pages/OrderPage/OrderPage.js
+++ src/pages/OrderPage/OrderPage.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { OrderContext } from "../../contexts/OrderContext";
 import Type from "./Type";
 
-function OrderPage() {
+function OrderPage({ setStep }) {
   const [orderDatas] = useContext(OrderContext);
   return (
     <div style={{ padding: "20px", textAlign: "left" }}>
@@ -17,7 +17,7 @@ function OrderPage() {
         <div>
           <h2>Total Price: {orderDatas.totals.total}</h2>
           <br />
-          <button>주문</button>
+          <button onClick={() => setStep(1)}>주문하기</button>
         </div>
       </div>
     </div>
